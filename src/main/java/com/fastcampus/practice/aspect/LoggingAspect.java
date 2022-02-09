26891ffe6d0a37c02c9ac1ab64b0f031623a0b44
G1.Jeon <g1.tommy.jeon@gmail.com>
2022-02-09 16:30:52 +0900

    feat: Apply Spring AOP
    - Controller 대상
    - 소요 시간 로깅

---
+++ src/main/java/com/fastcampus/practice/aspect/LoggingAspect.java
@@ -0,0 +1,31 @@
+package com.fastcampus.practice.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Aspect
+@Component
+public class LoggingAspect {
+
+    @Pointcut("within(com.fastcampus.practice.controller..*)")
+    public void onRequest() {}
+
+    @Around("com.fastcampus.practice.aspect.LoggingAspect.onRequest()")
+    public Object doLogging(ProceedingJoinPoint pjp) throws Throwable {
+        long start = System.currentTimeMillis();
+
+        try {
+            return pjp.proceed(pjp.getArgs());
+        } finally {
+            long end = System.currentTimeMillis();
+            log.warn("걸린시간: {}ms", end - start);
+        }
+
+    }
+
+}
